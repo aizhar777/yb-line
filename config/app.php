36a2a8b050abe81466ce3fc08eace0fbbff6342e
Aizhar777 <aizhar777@mail.ru>
2016-11-13 11:25:37 +0500

    Added package Sentinel: Sentry Implementation for Laravel

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Sentinel\SentinelServiceProvider::class, //https://github.com/rydurham/Sentinel
 
         //
 
